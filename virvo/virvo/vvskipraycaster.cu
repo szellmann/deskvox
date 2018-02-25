f13e619973fb8572f0af0817f9b85da9443e1424
Stefan Zellmann <info@szellmann.de>
2018-02-25 13:30:16 -0500

    Revert back to flat data structure

--- virvo/virvo/vvskipraycaster.cu
+++ virvo/virvo/vvskipraycaster.cu
@@ -263,14 +263,12 @@ void SVT<T>::build()
 
 
 //-------------------------------------------------------------------------------------------------
-// n-level SVT
+// two-level SVT
 //
 
-template <int N = 3>
-struct NLSVT
+struct TLSVT
 {
-    typedef SVT<uint16_t> svt16_t;
-    typedef SVT<uint32_t> svt32_t;
+    typedef SVT<uint16_t> svt_t;
 
     void reset(vvVolDesc const& vd, aabbi bbox, int channel = 0);
 
@@ -281,51 +279,35 @@ struct NLSVT
 
     vec3i bricksize = vec3i(32, 32, 32);
 
-    // Per level svt count (0 == bottom!)
-    vec3i num_svts[N];
-    // Bottom level svt (16-bit!)
-    std::vector<svt16_t> svts0;
-    // Higher precision for higher levels
-    std::vector<svt32_t> svts1;
-    std::vector<svt32_t> svts2;
+    vec3i num_svts;
+    std::vector<svt_t> svts;
 };
 
-template <int N>
-void NLSVT<N>::reset(vvVolDesc const& vd, aabbi bbox, int channel)
+void TLSVT::reset(vvVolDesc const& vd, aabbi bbox, int channel)
 {
-    num_svts[0] = vec3i(div_up(bbox.max.x, bricksize.x),
-                        div_up(bbox.max.y, bricksize.y),
-                        div_up(bbox.max.z, bricksize.z));
+    num_svts = vec3i(div_up(bbox.max.x, bricksize.x),
+                     div_up(bbox.max.y, bricksize.y),
+                     div_up(bbox.max.z, bricksize.z));
 
-    for (int n = 1; n < N; ++n)
-    {
-        num_svts[n] = vec3i(div_up(num_svts[n-1].x, bricksize.x),
-                            div_up(num_svts[n-1].y, bricksize.y),
-                            div_up(num_svts[n-1].z, bricksize.z));
-    }
-
-    svts0.resize(num_svts[0].x * num_svts[0].y * num_svts[0].z);
-    svts1.resize(num_svts[1].x * num_svts[1].y * num_svts[1].z);
-    svts2.resize(num_svts[2].x * num_svts[2].y * num_svts[2].z);
-    //...
+    svts.resize(num_svts.x * num_svts.y * num_svts.z);
 
 
-    // Fill level 0 with volume channel values
+    // Fill with volume channel values
 
     int bz = 0;
-    for (int z = 0; z < num_svts[0].z; ++z)
+    for (int z = 0; z < num_svts.z; ++z)
     {
         int by = 0;
-        for (int y = 0; y < num_svts[0].y; ++y)
+        for (int y = 0; y < num_svts.y; ++y)
         {
             int bx = 0;
-            for (int x = 0; x < num_svts[0].x; ++x)
+            for (int x = 0; x < num_svts.x; ++x)
             {
                 vec3i bmin(bx, by, bz);
                 vec3i bmax(min(bbox.max.x, bx + bricksize.x),
                            min(bbox.max.y, by + bricksize.y),
                            min(bbox.max.z, bz + bricksize.z));
-                svts0[z * num_svts[0].x * num_svts[0].y + y * num_svts[0].x + x].reset(vd, aabbi(bmin, bmax), channel);
+                svts[z * num_svts.x * num_svts.y + y * num_svts.x + x].reset(vd, aabbi(bmin, bmax), channel);
 
                 bx += bricksize.x;
             }
@@ -335,69 +317,21 @@ void NLSVT<N>::reset(vvVolDesc const& vd, aabbi bbox, int channel)
 
         bz += bricksize.z;
     }
-
-
-    // No channel values stored at higher levels
-    for (size_t i = 0; i < svts1.size(); ++i)
-        svts1[i].reset(aabbi(vec3i(0), bricksize));
-    for (size_t i = 0; i < svts2.size(); ++i)
-        svts2[i].reset(aabbi(vec3i(0), bricksize));
-    //...
 }
 
-template <int N>
 template <typename Tex>
-void NLSVT<N>::build(Tex transfunc)
+void TLSVT::build(Tex transfunc)
 {
     #pragma omp parallel for
-    for (size_t i = 0; i < svts0.size(); ++i)
+    for (size_t i = 0; i < svts.size(); ++i)
     {
-        svts0[i].apply(transfunc);
-        svts0[i].build();
+        svts[i].apply(transfunc);
+        svts[i].build();
     }
-
-    if (N < 2)
-        return;
-
-    #pragma omp parallel for
-    for (size_t i = 0; i < svts0.size(); ++i)
-    {
-        size_t j = 0;//i / (svts1[i].width * svts1[i].height * svts1[i].depth);
-        int x = i % num_svts[0].x;
-        int y = (i / num_svts[0].x) % num_svts[0].y;
-        int z = ((i / num_svts[0].x) / num_svts[0].y) % num_svts[0].z;
-        svts1[j].at(x, y, z) = svts0[i].last();
-    }
-
-    #pragma omp parallel for
-    for (size_t i = 0; i < svts1.size(); ++i)
-    {
-        svts1[i].build();
-    }
-
-    if (N < 3)
-        return;
-
-    #pragma omp parallel for
-    for (size_t i = 0; i < svts1.size(); ++i)
-    {
-        size_t j = i / (svts1[i].width * svts1[i].height * svts1[i].depth);
-        size_t k = i % svts1.size();
-        svts2[j].data()[k] = svts1[i].last();
-    }
-
-    #pragma omp parallel for
-    for (size_t i = 0; i < svts2.size(); ++i)
-    {
-        svts2[i].build();
-    }
-    //...
 }
 
-template <int N>
-uint64_t NLSVT<N>::get_count(aabbi bounds) const
+uint64_t TLSVT::get_count(aabbi bounds) const
 {
-#define CLIMB 1 // Climb up the hierarchy
     vec3i min_brick = bounds.min / bricksize;
     vec3i min_bpos = bounds.min - min_brick * bricksize;
 
@@ -418,29 +352,16 @@ uint64_t NLSVT<N>::get_count(aabbi bounds) const
 
             for (int bx = min_brick.x; bx <= max_brick.x; ++bx)
             {
-#if CLIMB
-                if (bx > min_brick.x && bx < max_brick.x && by > min_brick.y && by < max_brick.y && bz > min_brick.z && bz < max_brick.z)
-                {
-                    bx = max(min_brick.x, max_brick.x - 1);
-                    continue;
-                }
-#endif
-
                 int minx = bx == min_brick.x ? min_bpos.x : 0;
                 int maxx = bx == max_brick.x ? max_bpos.x : bricksize.x;
 
                 // 32**3 bricks can store 16 bit values, but the
                 // overall count will generally not fit in 16 bits
-                count += static_cast<uint64_t>(svts0[bz * num_svts[0].x * num_svts[0].y + by * num_svts[0].x + bx].get_count(
+                count += static_cast<uint64_t>(svts[bz * num_svts.x * num_svts.y + by * num_svts.x + bx].get_count(
                         aabbi(vec3i(minx, miny, minz), vec3i(maxx, maxy, maxz))));
             }
         }
     }
-
-#if CLIMB
-    if (min_brick.x < max_brick.x && min_brick.y < max_brick.y && min_brick.z < max_brick.z)
-        count += svts1[0/*TODO*/].get_count(aabbi(min_brick+vec3i(1), max_brick));
-#endif
 
     return count;
 }
@@ -494,7 +415,7 @@ struct KdTree
         }
     }
 
-    NLSVT<2> hsvt;
+    TLSVT hsvt;
 
     NodePtr root = nullptr;
 
